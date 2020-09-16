44c3b75fa0187cd7ad30c385a662aea1217b1f44
Saksham27 <geekysaksham27@gmail.com>
2020-09-17 04:40:01 +0530

    Updates

---
+++ precache-manifest.7b43a82fcbc039e6b836c89fd1d28fc8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22efecb789a2761eb501093584d6176d",
+    "url": "/liberty-ui/index.html"
+  },
+  {
+    "revision": "9c2c0a1997e9053c54d5",
+    "url": "/liberty-ui/static/css/main.db7da3d1.chunk.css"
+  },
+  {
+    "revision": "5be4af8c7ed496cd9fce",
+    "url": "/liberty-ui/static/js/2.5bc8cb51.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/liberty-ui/static/js/2.5bc8cb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2c0a1997e9053c54d5",
+    "url": "/liberty-ui/static/js/main.c2be551e.chunk.js"
+  },
+  {
+    "revision": "990aa1ba8cbf5944a19b",
+    "url": "/liberty-ui/static/js/runtime-main.e1bdbc66.js"
+  },
+  {
+    "revision": "e79698174ea9a96f79b6978116411b9a",
+    "url": "/liberty-ui/static/media/Employees.e7969817.PNG"
+  },
+  {
+    "revision": "8713871fdb8713ed3e0db4c02138b7f2",
+    "url": "/liberty-ui/static/media/UI_screenshot.8713871f.png"
+  },
+  {
+    "revision": "53cc269c73403e2c274d86c93e61ae74",
+    "url": "/liberty-ui/static/media/developer-image.53cc269c.jpg"
+  },
+  {
+    "revision": "d27dd4b4d2d1efac23e625a32f8eb6a6",
+    "url": "/liberty-ui/static/media/logo-full.d27dd4b4.svg"
+  },
+  {
+    "revision": "bf733e7429975eae405b3ae8d79be2a4",
+    "url": "/liberty-ui/static/media/logo-mini.bf733e74.svg"
+  },
+  {
+    "revision": "ae80a6daf111837654b04ef847d9c95d",
+    "url": "/liberty-ui/static/media/newFeedbacks.ae80a6da.PNG"
+  },
+  {
+    "revision": "487b7f9a4f5f9a03919c4e8e0fdb1988",
+    "url": "/liberty-ui/static/media/newUsers.487b7f9a.PNG"
+  },
+  {
+    "revision": "85bb3a9a90c4cd42c8b7454c19740248",
+    "url": "/liberty-ui/static/media/totalSales.85bb3a9a.PNG"
+  },
+  {
+    "revision": "53271aa5796a7f725aa415f1401df5ae",
+    "url": "/liberty-ui/static/media/user-image1.53271aa5.jpg"
+  },
+  {
+    "revision": "af8d5fedfda78e72755d174006491a3c",
+    "url": "/liberty-ui/static/media/user-image2.af8d5fed.jpg"
+  },
+  {
+    "revision": "d5afaa667746492dad279ef18667e623",
+    "url": "/liberty-ui/static/media/user-image3.d5afaa66.jpg"
+  }
+]);
